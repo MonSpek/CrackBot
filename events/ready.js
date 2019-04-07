178e6f3eded30d5367d63e1646bf481f8eb1d7de
ButchBullly <monspek@protonmail.com>
2019-04-07 09:19:32 -0400

    updates activity

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,7 @@ module.exports = async (client) => {
     let pNonPSer = (client.guilds.size > 1) ? 'Servers' : 'Server';
     let pNonPUsr = (client.users.size > 1) ? 'Users' : 'User';
     console.log(clc.yellow(`\n\n${client.user.username} is online\nOperating on ${client.guilds.size} ${pNonPSer}. \nUsed by ${client.users.size} ${pNonPUsr}.\n\n`)); //* logs on activity to console
-
-    client.user.setActivity(`${client.users.size} hustlas`, { type: "WATCHING" }) //Sets a status
+    client.setInterval(() => {
+        client.user.setActivity(`${client.users.size} hustlas`, { type: "WATCHING" }) //Sets a status
+    }, 60000);
 }
